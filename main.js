95550c4cbc45fc7204bae690b0c3689c36b15de4
ahmidibrahim <ahmidibrahim@gmail.com>
2024-12-12 05:25:46 +0200

    lesson 7

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
-// Some Var
+/* global  console, alert */
 
-var //Some var
-    x = 10,
-    y = 20,
-    z = 100;
+/* 
+    Start With Letters, Underscore, $
+
+    var = JavaScript Variable Keyword
+
+    myPrice = Variable Name
+
+    ( = ) = Assignment Operator
+
+    100 = Variable Value
+*/
+
+var // My Product Prices
+    myOldPrice = 2000,
+
+    myNewPrice = 900, 
+
+    myDiscount = myOldPrice - myNewPrice + 500; // 2000 - 900 + 500= 1600
 
 
-// Simple Function
 
-ducoment.getElementById("test").innerHTML = 100; // Simple Function
+document.getElementById("price").innerHTML = myDiscount;
