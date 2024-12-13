279a7373d6e09b0180a2f8336ca682b2aadedb9a
ahmidibrahim <ahmidibrahim@gmail.com>
2024-12-13 16:48:07 +0200

    lesso444

--- main.js
+++ main.js
@@ -1,24 +1,17 @@
 /* global  console, alert */
 
-/* 
+ 
     Start With Letters, Underscore, $
 
-    var = JavaScript Variable Keyword
+var = JavaScript Variable Keyword
 
-    myPrice = Variable Name
+myPrice = Variable Name
 
-    ( = ) = Assignment Operator
+    ( =) = Assignment Operator
 
-    100 = Variable Value
-*/
-
-var // My Product Prices
-    myOldPrice = 2000,
-
-    myNewPrice = 900, 
-
-    myDiscount = myOldPrice - myNewPrice + 500; // 2000 - 900 + 500= 1600
+100 = Variable Value
 
 
+var mainPrice = 500;
 
-document.getElementById("price").innerHTML = myDiscount;
+// document.getElementById("price").innerHTML = mainPrice;
